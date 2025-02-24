ebbb37c133a35192cb090178d6f2ee71d460a1d0
i18n <i18n.site@gmail.com>
2025-02-24 13:31:46 +0800

    ♨

--- gen/js/api.js
+++ gen/js/api.js
@@ -11,11 +11,11 @@ const [
   _set,
   _noArgs,
   _req
-] = hpc(T0Encode, T0Decode)
+] = hpc(T0Encode, T0Decode), NULL = ()=>{}
 
 export const set = _set;
 
 export const captcha = _noArgs(1,T1Decode) /* id:Box<[u8]>,img:Box<[u8]>,tip:Box<[u8]> */
-export const authSigninMail = (address /* str */,password /* str */)=>_req(2,T2Encode([address,password]))
-export const authSignupMail = (address /* str */,password /* str */)=>_req(3,T2Encode([address,password]))
+export const authSigninMail = (address /* str */,password /* str */)=>_req(2,NULL,T2Encode([address,password]))
+export const authSignupMail = (address /* str */,password /* str */)=>_req(3,NULL,T2Encode([address,password]))
 export const authSignupMailVerify = (address /* str */,code /* str */)=>_req(4,T3Decode,T2Encode([address,code])) /* SIGNUP_MAIL_VERIFY:enum */
