ad15c668993cdb486e4d5f532c9c585677a5777a
i18n <i18n.site@gmail.com>
2025-02-25 15:04:38 +0800

    ♨

--- gen/js/api.js
+++ gen/js/api.js
@@ -1,6 +1,6 @@
 import {
-  T2Encode,
-  T1Decode,T3Decode,
+  T2Encode,T4Encode,
+  T3Decode,T1Decode,T5Decode,
   T0Encode, //CallLiEncode,
   T0Decode // BinLiDecode
 } from './_.pb.js'
@@ -16,6 +16,7 @@ const [
 export const set = _set;
 
 export const captcha = _noArgs(1,T1Decode) /* id:Box<[u8]>,img:Box<[u8]>,tip:Box<[u8]> */
-export const authSigninMail = (address /* str */,password /* str */)=>_req(2,NULL,T2Encode([address,password]))
-export const authSignupMail = (address /* str */,password /* str */)=>_req(3,NULL,T2Encode([address,password]))
-export const authSignupMailVerify = (address /* str */,code /* str */)=>_req(4,T3Decode,T2Encode([address,code])) /* SIGNUP_MAIL_VERIFY:enum */
+export const authSigninMail = (address /* str */,password /* str */)=>_req(2,NULL,T4Encode([address,password]))
+export const authSignupMail = (address /* str */,password /* str */)=>_req(3,NULL,T4Encode([address,password]))
+export const authSignupMailVerify = (address /* str */,code /* str */)=>_req(4,T5Decode,T4Encode([address,code])) /* SIGNUP_MAIL_VERIFY:enum */
+export const captchaVerify = (id /* [u8] */,click_pos_li /* [u32] */)=>_req(5,T3Decode,T2Encode([id,click_pos_li])) /* :string */
